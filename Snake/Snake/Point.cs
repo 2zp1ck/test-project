bc4879f03beeede5707f11626f23e7ac60a2a265
2zp1ck <2zp1ck87@gmail.com>
2016-04-12 16:02:02 +0300

    создали класс Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point //класс для обозначения точки (класс может состоять из данных и функций)
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw() //функция для вывода символа на экран (входные параметры - координаты и вид символа)
+        {
+            Console.SetCursorPosition(x, y); //задаем координаты точки
+            Console.Write(sym); //выводим переданный символ в данной позиции
+        }
+    }
+}
